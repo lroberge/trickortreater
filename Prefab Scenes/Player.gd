94061547eb625f36fa4bf91e743b8a14047a417e
Mason <37517085+ItsTonus@users.noreply.github.com>
2021-10-30 17:11:50 -0400

    Started adding sugar rush and tried making its GUI bar

--- Prefab Scenes/Player.gd
+++ Prefab Scenes/Player.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 # This variable is exported, and can be edited in the Inspector
 # Speed, in maximum pixels per second
-export var speed = 200
+export var speed = 400
+var sugarRush = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,6 +23,8 @@ func _physics_process(delta):
 	if(Input.is_action_pressed("player_right")):
 		velocity.x += 1
 	
-	velocity = velocity.normalized() * speed
+	velocity = velocity.normalized() * speed * max(sugarRush, 0.25)
 	
 	move_and_slide(velocity)
+	
+	sugarRush -= 0.01*delta
